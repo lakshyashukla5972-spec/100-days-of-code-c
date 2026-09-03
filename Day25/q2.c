068cebc9b2771e8b54b7aca9ee57c7bf8e0b6998
Lakshya Shukla <lakshyashukla5972@gmail.com>
2026-09-03 22:03:08 +0530

    working

---
+++ Day25/q2.c
@@ -0,0 +1,39 @@
+//Q50: Write a program to print the following pattern:
+/*
+*****
+ ****
+  ***
+   **
+    *
+*/
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*****
+ ****
+  ***
+   **
+    *
+
+Input 2:
+
+Output 2:
+Note: Spaces indicate indentation.
+
+*/
+#include <stdio.h>
+int main() {
+    int n = 5; // Number of rows
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            printf(" ");
+        }
+        for (int k = 0; k < n - i; k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
